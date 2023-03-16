60a613b4108ada2fa265e136ea82457c4ce33e86
Juan Pablo Alba Fernandez <juanpialba@gmail.com>
2023-03-16 19:39:54 -0300

    agregamos cambio a ejercicio 6

--- js/05-ejercicio5.js
+++ js/05-ejercicio5.js
@@ -13,86 +13,93 @@ Deberá de repetirse el proceso hasta que el usuario pulse «cancelar».
 
 Ejemplo: si ingresamos el DNI 40773821, la salida debería ser ‘L’
 */
-let letra = '';
+let letra = "";
 let resto = 0;
 let number = 0;
-do{
-    number = prompt('Ingrese su numero de Dni')
-    if(number > 0 || number < 99999999){        
-        resto = numero%23
-        switch(resto){
-            case 0:
-            letra = 't';
-            break;
-            case 1:
-            letra = 'r'
-            break;
-            case 2:
-            letra = 'w'
-            break;
-            case 3:
-            letra = 'w'
-            break;
-            case 4:
-            letra = 'w'
-            break;
-            case 5:
-            letra = 'w'
-            break;
-            case 6:
-            letra = 'w'
-            break;
-            case 7:
-            letra = 'w'
-            break;
-            case 8:
-            letra = 'w'
-            break;
-            case 9:
-            letra = 'w'
-            break;
-            case 10:
-            letra = 'w'
-            break;
-            case 11:
-            letra = 'w'
-            break;
-            case 12:
-            letra = 'w'
-            break;
-            case 13:
-            letra = 'w'
-            break;
-            case 14:
-            letra = 'w'
-            break;
-            case 15:
-            letra = 'w'
-            break;
-            case 16:
-            letra = 'w'
-            break;
-            case 17:
-            letra = 'w'
-            break;
-            case 18:
-            letra = 'w'
-            break;
-            case 19:
-            letra = 'w'
-            break;
-            case 20:
-            letra = 'w'
-            break;
-            case 21:
-            letra = 'w'
-            break;
-            case 22:
-            letra = 'w'
-            break;           
-
-        }
-
-}
-
-}
+do {
+  number = prompt("Ingrese su numero de Dni");
+  if (Number(number) == number) {
+    number = Number(number);
+    if (number > 0 || number < 99999999) {
+      resto = number % 23;
+      switch (resto) {
+        case 0:
+          letra = "t";
+          break;
+        case 1:
+          letra = "r";
+          break;
+        case 2:
+          letra = "w";
+          break;
+        case 3:
+          letra = "w";
+          break;
+        case 4:
+          letra = "w";
+          break;
+        case 5:
+          letra = "w";
+          break;
+        case 6:
+          letra = "w";
+          break;
+        case 7:
+          letra = "w";
+          break;
+        case 8:
+          letra = "w";
+          break;
+        case 9:
+          letra = "w";
+          break;
+        case 10:
+          letra = "w";
+          break;
+        case 11:
+          letra = "w";
+          break;
+        case 12:
+          letra = "w";
+          break;
+        case 13:
+          letra = "w";
+          break;
+        case 14:
+          letra = "w";
+          break;
+        case 15:
+          letra = "w";
+          break;
+        case 16:
+          letra = "w";
+          break;
+        case 17:
+          letra = "w";
+          break;
+        case 18:
+          letra = "w";
+          break;
+        case 19:
+          letra = "w";
+          break;
+        case 20:
+          letra = "w";
+          break;
+        case 21:
+          letra = "w";
+          break;
+        case 22:
+          letra = "w";
+          break;
+        default:
+          alert("No es un numero");
+      }
+      alert("Numero:" + number + "Letra" + letra);
+    } else {
+      if (number != undefined) {
+        alert(number + " No es un numero");
+      }
+    }
+  }
+} while (number != undefined);
---
+++ js/06-ejercicio6.js
@@ -0,0 +1,10 @@
+
+
+var i = 0;
+var rep = 0;
+for (i = 1; i <= 0; i++) {
+  for (rep = 0; rep < i; rep++) {
+    document.write(i);
+  }
+  document.write("<br>");
+}
